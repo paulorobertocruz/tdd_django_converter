389b45519b7d122bbf37e376a73720c1634918f6
Paulo Roberto Cruz <paulo.cruz9@gmail.com>
2020-12-24 18:06:55 -0300

    teste para conversão de centimetro para metro

--- tdd_django_converter/length/tests.py
+++ tdd_django_converter/length/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# Create your tests here.
+
+class TestLengthConversion(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.url = reverse('length:convert')
+
+    def test_centimetre_to_metre_conversion(self):
+
+        data = {
+            "input_unit": "centimetre",
+            "output_unit": "metre",
+            "input_value": round(8096.894, 3),
+        }
+
+        response = self.client.get(self.url, data)
+
+        self.assertContains(response, 80.969)
